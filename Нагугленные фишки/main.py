41c1458b263c675cff88923870531640192be3bc
ervand7 <ervand7@mail.ru>
2020-12-05 15:37:54 +0300

    Coreecting spliiting dict funcrion

--- Нагугленные фишки/main.py
+++ Нагугленные фишки/main.py
@@ -52,19 +52,28 @@
 
 
 
-# # Разбить словарь на мелкие словари по запятым
-# # На выходе получаем список с мелкими словарями
-# # https://coderoad.ru/22878743/%D0%9A%D0%B0%D0%BA-%D0%B1%D1%8B%D1%81%D1%82%D1%80%D0%BE-%D1%80%D0%B0%D0%B7%D0%B1%D0%B8%D1%82%D1%8C-%D1%81%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D1%8C-%D0%BD%D0%B0-%D0%BD%D0%B5%D1%81%D0%BA%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE-%D1%81%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D0%B5%D0%B9
-# initial_dict = {'qwe': 123, 'asd': 345, 'zxc': 567}
+# # # Разбить словарь на мелкие словари по запятым
+# # # На выходе получаем список с мелкими словарями
+# # # https://coderoad.ru/22878743/%D0%9A%D0%B0%D0%BA-%D0%B1%D1%8B%D1%81%D1%82%D1%80%
+# # D0%BE-%D1%80%D0%B0%D0%B7%D0%B1%D0%B8%D1%82%D1%8C-%D1%81%D0%BB%D0%BE%D0%B2%D0%B0%D1%
+# # 80%D1%8C-%D0%BD%D0%B0-%D0%BD%D0%B5%D1%81%D0%BA%D0%BE%D0%BB%D1%8C%D0%BA%D0%BE-%D1%81%D0%BB%D0%BE%D0%B2%D0%B0%D1%80%D0
+# # %B5%D0%B9
 
-# from itertools import islice  
-# def chunks(data, size=0):
-#     it = iter(data)
-#     for i in range(0, len(data), size):
-#         yield {k: data[k] for k in islice(it, size)}
-
+# from itertools import islice
+#
+# my_big_initial_dict = {123: 'asd', 234: 'cvb', 645: 'pio', 468: 'xcvxcv'}
+#
+#
+# def splitting_big_dct_into_many_small_dicts(initial_dict, start=1):
+#     iter_initial_dict = iter(initial_dict)
+#     for i in range(len(initial_dict)):
+#         yield {key_: initial_dict[key_] for key_ in islice(iter_initial_dict, start)}
+#
+#
+# n = splitting_big_dct_into_many_small_dicts
+#
 # lst_of_small_dicts = []
-# for item in chunks({i: a for i, a in initial_dict.items()}, 1):
+# for item in n(my_big_initial_dict):  # for item in n({_key: _value for _key, _value in my_dict.items()}, 1)
 #     lst_of_small_dicts.append(item)
 # print(lst_of_small_dicts)
 # _______________________________________
